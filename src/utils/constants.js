068e549238cfa9e2adf9557a6029210e53eae721
practice <arpitsinghal471@gmail.com>
2024-09-28 17:02:32 +0530

    add cors proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,10 @@
 export const LIVE_CHAT_COUNT=25;
 
 
- export const YOUTUBE_VIDEOS_API= "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=8&regionCode=IN&key="+process.env.REACT_APP_YOUTUBE_KEY;
+ export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=8&regionCode=IN&key="+process.env.REACT_APP_YOUTUBE_KEY;
 
+ 
 
+export const YOUTUBE_SEARCH_API="https://thingproxy.freeboard.io/fetch/https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q=";
 
-export const YOUTUBE_SEARCH_API="https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q="
+ 
